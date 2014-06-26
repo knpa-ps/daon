2cb7fd6fb5f2681ca2f03123aca6839ffbec2d59
projet6634 <projet6634@gmail.com>
2014-06-26 15:00:35 +0900

    production setup

--- app/config/database.php
+++ app/config/database.php
@@ -55,33 +55,13 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'daon_db',
+			'username'  => 'daon',
+			'password'  => 'chzhvkdl',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
-		),
-
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'forge',
-			'username' => 'forge',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
-		),
+		)
 
 	),
 
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,6 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::group(['prefix'=>'api'], function() {
+	Route::resource('departments', 'DepartmentController');
 });
